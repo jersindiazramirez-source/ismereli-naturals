25444b0436bda90d35151f4f5f824730f8e26089
jersindiazramirez-source <jersindiazramirez@gmail.com>
2026-07-04 16:45:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+const buscador = document.getElementById("buscar");
+const productos = document.querySelectorAll(".producto");
+
+buscador.addEventListener("keyup", function () {
+  const texto = buscador.value.toLowerCase();
+
+  productos.forEach((producto) => {
+    const nombre = producto.querySelector("h3").textContent.toLowerCase();
+
+    if (nombre.includes(texto)) {
+      producto.style.display = "block";
+    } else {
+      producto.style.display = "none";
+    }
+  });
+});
